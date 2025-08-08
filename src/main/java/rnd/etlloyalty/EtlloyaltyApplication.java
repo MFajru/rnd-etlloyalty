bd881bedc20ba6529be70f0e09da1adf70c17825
MFajru <fajru234@gmail.com>
2025-08-08 16:59:51 +0700

    init

---
+++ src/main/java/rnd/etlloyalty/EtlloyaltyApplication.java
@@ -0,0 +1,13 @@
+package rnd.etlloyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlloyaltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlloyaltyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rnd/etlloyalty/EtlloyaltyApplicationTests.java
@@ -0,0 +1,13 @@
+package rnd.etlloyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtlloyaltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
