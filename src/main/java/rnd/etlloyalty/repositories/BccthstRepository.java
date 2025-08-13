ed340cf4a46c5e459bb7a7f7e11b1b655569c476
MFajru <fajru234@gmail.com>
2025-08-13 17:06:04 +0700

    feat: change query for cc transaction

--- src/main/java/rnd/etlloyalty/repositories/BccthstRepository.java
+++ src/main/java/rnd/etlloyalty/repositories/BccthstRepository.java
@@ -14,25 +14,37 @@ public interface BccthstRepository extends JpaRepository<BccthstRecord, String> 
     List<BccthstRecord> selectAllBccthst();
     // change query to left join semua table
     @Query(value = """
-                    SELECT 
-                        b.THST_APPROVAL_CODE as approvalCode,
-                        b.THST_CARD_NBR as cardNbr,
-                        b.THST_ORG_NBR as cardOrg,
-                        b.THST_TYPE as cardType,
-                        b.THST_MERCH_CAT as merchCat,
-                        b.THST_TRAN_CODE as tranCode,
-                        b.THST_TRAN_AMT as tranAmount,
-                        b.THST_TRAN_DATE as tranDate,
-                        o.OASA_MERCHANT_NBR as merchantNbr,
-                        o.OASA_MERCHANT_ORG as merchantOrg,
-                        o.OASA_B019_CNTRY_CODE as countryCode
-                    FROM
-                        EPURSEPLUS.RND_CRDLNK_BCCTHST b
-                    LEFT JOIN
-                        EPURSEPLUS.RND_ODS_CRDLNK_OASLOG o ON b.THST_APPROVAL_CODE = o.OASA_B038_AUTH_CODE
-                    LEFT JOIN 
-                        EPURSEPLUS.RND
-                    """,
+            SELECT b.THST_APPROVAL_CODE AS approvalCode, b.THST_ORG_NBR , b.THST_CARD_NBR AS cardNumber,
+                    b.THST_TYPE AS cardType, b.THST_MERCH_CAT AS merchantCat, os.OASA_MERCHANT_ORG AS merchantOrg, os.OASA_MERCHANT_NBR AS merchantId,
+                    CASE
+                    	WHEN bn.BNICA_F038_AUTH_CDE IS NOT NULL THEN 'Wondr'
+                    	WHEN o.OADCL_APPROVAL_CODE IS NOT NULL THEN 'EDC'
+                    	ELSE 'Unknown'
+                    END AS channel,
+                    CONCAT(os.OASA_B041_CRD_ACCPT_STORE, os.OASA_B041_CRD_ACCPT_TERM) AS terminalId ,  b.THST_TRAN_AMT AS tranAmount,
+                    b.THST_TRAN_CODE AS tranCode,
+                    CASE\s
+                    	WHEN b.THST_TRAN_CODE = '40' THEN 'RETAIL PURCHASE'
+                    	WHEN b.THST_TRAN_CODE = '41' THEN 'REFUND RETAIL PURCHASE'
+                    	WHEN b.THST_TRAN_CODE = '65' THEN 'CREDIT CARD INSURANCE'
+                    	WHEN b.THST_TRAN_CODE = '66' THEN 'REFUND CREDIT CARD INSURANCE'
+                    	ELSE 'Unknown'
+                    END AS tranCodeDesc,
+                    CONCAT(
+                    	CASE
+                    		WHEN INSTR(bn.BNICA_F060_BANK_HEADER, 'APISPC') > 0 THEN 'BILL PAYMENT-'
+                    		ELSE 'PURCHASE-'
+                    	END,
+                    	b.THST_DESCRIPTION
+                    ) AS tranFeature,
+                    b.THST_TRAN_DATE AS tranDate,
+                    SUBSTR(bn.BNICA_F060_BANK_HEADER, INSTR(bn.BNICA_F060_BANK_HEADER, 'APISPC') + 6, 4) AS utilCode,
+                    os.OASA_B019_CNTRY_CODE AS countryCode
+            FROM RND_ODS_CRDLNK_BCCTHST b
+            LEFT JOIN RND_ODS_CRDLNK_OASLOG os ON b.THST_APPROVAL_CODE = os.OASA_B038_AUTH_CODE\s
+            LEFT JOIN RND_ODS_CRDLNK_BNLOG_ATM bn ON b.THST_APPROVAL_CODE = bn.BNICA_F038_AUTH_CDE
+            LEFT JOIN RND_ODS_CRDLNK_OADCLOG o ON b.THST_APPROVAL_CODE = o.OADCL_APPROVAL_CODE;
+            """,
             nativeQuery = true)
     Any selectJoinBccthstOaslog();
 }
